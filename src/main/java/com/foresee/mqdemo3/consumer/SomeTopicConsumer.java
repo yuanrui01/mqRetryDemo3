05f8da75bafb16328ada63cd27ef17c8a37f2042
yuanrui <xdyrfree@gmail.com>
2023-11-03 09:17:05 +0800

    补充测试代码，晚点回家验证

--- src/main/java/com/foresee/mqdemo3/consumer/SomeTopicConsumer.java
+++ src/main/java/com/foresee/mqdemo3/consumer/SomeTopicConsumer.java
@@ -1,5 +1,6 @@
 package com.foresee.mqdemo3.consumer;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.common.UtilAll;
@@ -17,7 +18,7 @@ import java.util.Optional;
  * @author: yuanrui
  */
 @RocketMQMessageListener(topic = "someTopic",
-        consumerGroup = "cg")
+        consumerGroup = "${rocketmq.consumer.group.simbaOpenapiImport}")
 @Component
 @Slf4j
 public class SomeTopicConsumer implements RocketMQListener<MessageExt>, RocketMQPushConsumerLifecycleListener {
@@ -29,13 +30,17 @@ public class SomeTopicConsumer implements RocketMQListener<MessageExt>, RocketMQ
         defaultMQPushConsumer.setConsumeTimestamp(UtilAll.timeMillisToHumanString3(System.currentTimeMillis()));
     }
 
+    @SneakyThrows
     @Override
     public void onMessage(MessageExt message) {
         String taskId = message.getKeys();
         byte[] body = message.getBody();
         String value = new String(body, StandardCharsets.UTF_8);
 
-        log.info("Received message, taskId : {}, value : {}", taskId, value);
+        //log.info("Received message, taskId : {}, value : {}", taskId, value);
         log.info("Received message, message : {}", message);
+        if (true) {
+            throw new Exception("人工异常，不服憋着！");
+        }
     }
 }
--- src/main/java/com/foresee/mqdemo3/controller/HelloController.java
+++ src/main/java/com/foresee/mqdemo3/controller/HelloController.java
@@ -1,8 +1,16 @@
 package com.foresee.mqdemo3.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.foresee.mqdemo3.mq.RocketMQTemplateProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * @author: yuanrui
@@ -12,8 +20,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private RocketMQTemplateProducer rocketMQTemplateProducer;
+
+    @Value("${rocketmq.testTopic}")
+    private String topic;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/send/{id}")
+    public String send(@PathVariable String id) {
+        String msg = "message" + id;
+        String taskId = UUID.randomUUID().toString();
+        Message<String> message = MessageBuilder
+                .withPayload(msg)
+                .setHeader(RocketMQHeaders.KEYS, taskId)
+                .build();
+        SendResult sendResult = rocketMQTemplateProducer.syncSendMessage(topic, message);
+        if (SendStatus.SEND_OK.equals(sendResult.getSendStatus())) {
+            return "success";
+        } else {
+            return "error";
+        }
     }
 }
